f9cd41ddbce1b1baf30822d7b318040f67a1ea8e
carterhanks <carterhanks@gmail.com>
2021-08-05 11:40:26 -0600

    Up to Module 3

---
+++ src/app.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const accountData = fs.readFileSync('src/json/accounts.json', {encoding:'utf8'});
+const accounts = JSON.parse(accountData);
+const userData = fs.readFileSync('src/json/users.json', {encoding:'utf8'});
+const users = JSON.parse(userData);
+
+app.get('/', (req, res) => {
+    res.render("index", { title: 'Account Summary', accounts: accounts });
+});
+
+app.get('/savings', (req, res) => {
+    res.render("account", { title: 'Savings Summary', account: accounts.savings });
+});
+
+app.get('/checking', (req, res) => {
+    res.render("account", { title: 'Checking Summary', account: accounts.checking });
+});
+
+app.get('/credit', (req, res) => {
+    res.render("account", { title: 'Credit Summary', account: accounts.credit });
+});
+
+app.get('/transfer', (req, res) => {
+    res.render("transfer");
+});
+
+app.get('/profile', (req, res) => {
+    res.render("profile", { title: 'Profile', user: users[0] });
+});
+
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`PS Project Running on ${port}!`)
+});
